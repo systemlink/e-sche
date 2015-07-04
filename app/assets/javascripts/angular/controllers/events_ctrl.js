4dbca16d4deb0394df7321477c469ff5bdf192b6
ino_h <syslink.h.inoue@gmail.com>
2015-07-04 17:06:13 +0900

    候補日の一覧取得

--- app/assets/javascripts/angular/controllers/events_ctrl.js
+++ app/assets/javascripts/angular/controllers/events_ctrl.js
@@ -1,13 +1,18 @@
-/*global window,angular*/
+/*global console,angular*/
 (function () {
   'use strict';
 
   angular.module('esche')
     .controller("EventsCtrl.new", function ($scope, $location, eventService) {
+      $scope.dateFrom = new Date();
+      $scope.dateTo = new Date();
       $scope.create = function (event) {
         eventService.create(event, function (data) {
           $location.path("/events/" + data.event.id);
         });
+      };
+      $scope.addDates = function (dateFrom, dateTo) {
+        $scope.dates = eventService.getDates(dateFrom, dateTo);
       };
     })
     .controller("EventsCtrl.show", function ($scope, $routeParams, $location, eventService) {
--- app/assets/javascripts/angular/services/event.js
+++ app/assets/javascripts/angular/services/event.js
@@ -1,4 +1,4 @@
-/*global window,angular*/
+/*global console,alert,angular*/
 (function () {
   
   'use strict';
@@ -10,27 +10,42 @@
         $http.post("/api/events.json", args)
           .success(function (data) {
             if (data.error) {
-              window.alert(data.error);
+              alert(data.error);
             } else {
               success(data);
             }
           })
           .error(function (data, status, headers, config) {
-            window.console.log("error : " + status);
+            console.log("error : " + status);
           });
       };
       this.show = function (eventId, success) {
         $http.get("/api/events/" + eventId + ".json", {})
           .success(function (data, status, headers, config) {
             if (data.error) {
-              window.alert(data.error);
+              alert(data.error);
             } else {
               success(data);
             }
           })
           .error(function (data, status, headers, config) {
-            window.console.log("error");
+            console.log("error");
           });
+      };
+      this.getDates = function (inputFrom, inputTo) {
+        var
+          currentDate,
+          toDate,
+          dates = [];
+        if (angular.isDefined(inputFrom) && angular.isDefined(inputTo)) {
+          currentDate = new Date(inputFrom);
+          toDate = new Date(inputTo);
+          while (currentDate <= toDate) {
+            dates[dates.length] = new Date(currentDate.getFullYear(), currentDate.getMonth(), currentDate.getDate());
+            currentDate.setDate(currentDate.getDate() + 1);
+          }
+        }
+        return dates;
       };
     });
 
