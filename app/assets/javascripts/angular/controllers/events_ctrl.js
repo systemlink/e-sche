e910e8a31bb94c0d9731ec547c96e049117d3ccb
ino_h <syslink.h.inoue@gmail.com>
2015-07-06 06:06:52 +0900

    候補日入力対応

--- app/assets/javascripts/angular/controllers/events_ctrl.js
+++ app/assets/javascripts/angular/controllers/events_ctrl.js
@@ -4,15 +4,25 @@
 
   angular.module('esche')
     .controller("EventsCtrl.new", function ($scope, $location, eventService) {
+      $scope.event = {
+        title: "",
+        note: "",
+        dates: []
+      };
       $scope.dateFrom = new Date();
       $scope.dateTo = new Date();
+      $scope.dateTo.setDate($scope.dateTo.getDate() + 7);
       $scope.create = function (event) {
         eventService.create(event, function (data) {
           $location.path("/events/" + data.event.id);
         });
       };
       $scope.addDates = function (dateFrom, dateTo) {
-        $scope.dates = eventService.getDates(dateFrom, dateTo);
+        $scope.event.dates = eventService.getDates(dateFrom, dateTo);
+      };
+      $scope.addDates($scope.dateFrom, $scope.dateTo);
+      $scope.dateChange = function (index) {
+        $scope.event.dates.splice(index, 1);
       };
     })
     .controller("EventsCtrl.show", function ($scope, $routeParams, $location, eventService) {
