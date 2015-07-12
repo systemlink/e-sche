ab11bc6796ca4fa41f0df06f3b4ac837161588e9
ino_h <syslink.h.inoue@gmail.com>
2015-07-12 09:51:07 +0900

    テストコード追加

--- app/mailers/event_mailer.rb
+++ app/mailers/event_mailer.rb
@@ -1,5 +1,5 @@
 class EventMailer < ApplicationMailer
   def notification(from_address, to_addresses, event)
-    mail to: to_addresses, from: from_address, subject: event.title
+    mail to: to_addresses, from: from_address, subject: event.title, body: ""
   end
 end
--- spec/mailers/event_mailer_spec.rb
+++ spec/mailers/event_mailer_spec.rb
@@ -1,5 +1,23 @@
 require "rails_helper"
 
 RSpec.describe EventMailer, type: :mailer do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:from_address) { "from_address@host.com" }
+  let(:to_addresses) { "to_addresses@host.com" }
+  
+  before do
+    @event = create(:event, :full)
+    @event_mailer = EventMailer.notification(from_address, to_addresses, @event)
+  end
+
+  it "送信元アドレスが正しいこと" do
+    expect(@event_mailer.from).to eq [from_address]
+  end
+  
+  it "送信先アドレスが正しいこと" do
+    expect(@event_mailer.to).to eq [to_addresses]
+  end
+  
+  it "タイトルが正しいこと" do
+    expect(@event_mailer.subject).to eq @event.title
+  end
 end
